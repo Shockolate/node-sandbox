a731d1ffd781f288c223815ee69398cb68512f4c
Ted Armstrong <theodorecarmstrong@gmail.com>
2020-12-07 14:28:23 -0500

    fix: starting point

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const uuid = require('uuid');
-const axios = require('axios');
-
-const username = process.env.CFS_USER;
-const password = process.env.CFS_PW;
-const site = process.env.SITE;
 
 async function main() {
-  try {
-    const response = await axios.get(`https://${username}:${password}@${site}.6river.org`);
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 
