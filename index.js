64d69fd2c25f3e3daef33f7b3f512d6f8a14b1df
Ted Armstrong <theodorecarmstrong@gmail.com>
2020-12-10 16:43:17 -0500

    fix: advent2020 day 4

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+// eslint-disable-next-line strict
+'use strict';
 
-async function main() {
+require('source-map-support').install();
+
+const application = require('./dist');
+
+module.exports = application;
+
+if (require.main === module) {
+	// Run the application
+	application.main().catch((err) => {
+		// eslint-disable-next-line no-console
+		console.error('Cannot start the application.', err);
+		process.exit(1);
+	});
 }
-
-
-main().catch(console.error);
