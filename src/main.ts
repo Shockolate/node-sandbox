9224e3aff7ab2154441a8e887a0d3dd3b4f40530
Ted Armstrong <theodorecarmstrong@gmail.com>
2021-11-01 12:54:34 -0400

    feat: modernize sandbox

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { text } from './input';
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-console */
+// import axios from 'axios';
+// import { v4 as uuidv4 } from 'uuid';
 
-export async function main() {
-
+export async function main(): Promise<void> {
+	console.log('Hello world');
 }
