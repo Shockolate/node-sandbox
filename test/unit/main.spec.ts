8e7b117c217bd740bb36586e30f5dae9295e870d
Ted Armstrong <theodorecarmstrong@gmail.com>
2022-03-03 15:54:27 -0500

    feat: adds unit test framework

---
+++ test/unit/main.spec.ts
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+
+// import { main } from '../../src/main';
+
+describe('main', function() {
+  it('should work', async function() {
+    // await main();
+    assert.isTrue(true);
+  })
+});
